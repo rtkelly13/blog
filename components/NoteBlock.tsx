b5f8f293b188e4d1964336a4d8a5273895d99080
Ryan Kelly <94ryan.kelly@gmail.com>
2026-07-26 23:14:31 +0100

    chore: bind @rtkelly/design-system as relative peer repository dependency for fast local tandem dev

--- components/NoteBlock.tsx
+++ components/NoteBlock.tsx
@@ -1,4 +1,5 @@
-import type { ReactNode } from 'react';
+import { NoteBlock as SystemNoteBlock } from '@rtkelly/design-system';
+import React, { type ReactNode } from 'react';
 
 interface NoteBlockProps {
   children: ReactNode;
@@ -11,29 +12,23 @@ const NoteBlock = ({
   title = 'Note',
   color = 'primary',
 }: NoteBlockProps) => {
-  const colors = {
-    primary: 'bg-black border-brutalist-cyan text-white',
-    red: 'bg-black border-brutalist-pink text-white',
-    blue: 'bg-black border-brutalist-cyan text-white',
-    green: 'bg-black border-white text-white',
-    yellow: 'bg-black border-brutalist-yellow text-white',
-    gray: 'bg-zinc-900 border-white text-white',
+  const getType = () => {
+    switch (color) {
+      case 'red':
+        return 'warning';
+      case 'yellow':
+        return 'important';
+      case 'green':
+        return 'tip';
+      default:
+        return 'note';
+    }
   };
 
-  const colorClasses = colors[color] || colors.primary;
-
   return (
-    <div
-      className={`${colorClasses} border-l-4 border-t-2 border-r-2 border-b-2 px-3 py-2 my-4 font-mono`}
-      role="alert"
-    >
-      <div className="flex">
-        <div>
-          <p className="font-bold uppercase">[ {title} ]</p>
-          <div className="text-sm">{children}</div>
-        </div>
-      </div>
-    </div>
+    <SystemNoteBlock type={getType()} title={title}>
+      {children}
+    </SystemNoteBlock>
   );
 };
 
