779011debf6395e6c4429d99650adfe295d617cd
Ryan Kelly <94ryan.kelly@gmail.com>
2026-07-27 16:29:29 +0100

    feat(design-system): use DS preset in tailwind.config.js and swap Button, Card, Tag, PageTitle, BracketText to @rtkelly13/design-system (#92)

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -1,14 +1,2 @@
-import type { ReactNode } from 'react';
-import BracketText from './BracketText';
-
-interface Props {
-  children: ReactNode;
-}
-
-export default function PageTitle({ children }: Props) {
-  return (
-    <h1 className="text-3xl font-display font-bold leading-tight tracking-tight text-white uppercase sm:text-4xl md:text-5xl border-4 border-double border-white inline-block px-6 py-4">
-      <BracketText>{children}</BracketText>
-    </h1>
-  );
-}
+export type { PageTitleProps } from '@rtkelly13/design-system';
+export { PageTitle as default } from '@rtkelly13/design-system';
